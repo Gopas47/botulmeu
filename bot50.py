e2dc231a71eb587d0d5188e595a77e9cd501bfe5
Gopas47 <45892900+Gopas47@users.noreply.github.com>
2018-12-21 22:08:15 +0200

    Update bot50.py

--- bot50.py
+++ bot50.py
@@ -12,6 +12,15 @@ client = commands.Bot(command_prefix = '!')
 Clientdiscord = discord.Client()
 
 
+@Client.command(pass_context = True)
+async def clear(ctx, number):
+    mgs = [] #Empty list to put all the messages in the log
+    number = int(number) #Converting the amount of messages to delete to an integer
+    async for x in Client.logs_from(ctx.message.channel, limit = number):
+        mgs.append(x)
+    await Client.delete_messages(mgs)
+
+
 @client.event
 async def on_member_join(member):
     await client.send_message(member, 'Salut , bine ai venit pe serverul nostru de Discord , bafta !')
