643ce597dfbce359b804c51ab13ecb6a6c49307a
Gopas47 <45892900+Gopas47@users.noreply.github.com>
2019-01-01 15:00:46 +0200

    Update bot50.py

--- bot50.py
+++ bot50.py
@@ -6,13 +6,16 @@ import time
 import random
 from discord import Game
 
-
 Client = discord.client
 client = commands.Bot(command_prefix = '!')
 Clientdiscord = discord.Client()
-async def change_status(self):
-    await self.bot.change_presence(game=discord.Game(name="seBaKa"))
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='seBaKa!'))
     print('Ready, Freddy') 
+
+
 
 
 @client.event
